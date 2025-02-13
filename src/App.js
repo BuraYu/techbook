b2bc1efbe41266b1865e3f5bc91d57516eb95e81
BuraYu <burak.yueksel@hotmail.com>
2025-02-13 14:23:50 +0100

    add css variables, reset, typo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Hello World</h1>
+      <h1 className="text-preset-1"> Hello World</h1>
+      <h1 className="text-preset-2"> Hello World</h1>
+      <h1 className="text-preset-3"> Hello World</h1>
+      <h1 className="text-preset-6"> Hello World</h1>
     </div>
   );
 }
