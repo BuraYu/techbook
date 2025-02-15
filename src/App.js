485c9a2ddbf85d7ab10f09d9b67c2f8fde9d355d
BuraYu <burak.yueksel@hotmail.com>
2025-02-15 21:17:43 +0100

    add secondary container component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import MainContainer from "./components/MainContainer";
+import SecondaryContainer from "./components/SecondaryContainer";
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <MainContainer />
+      <SecondaryContainer />
     </div>
   );
 }
---
+++ src/components/SecondaryContainer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../App.css";
+import styles from "./secondaryContainer.module.css";
+import notAverageReaders from "../assets/image-not-average-desktop.webp";
+import circle from "../assets/pattern-circle.png";
+
+const SecondaryContainer = () => {
+  return (
+    <div className={styles.second_container_wrapper}>
+      <div className={styles.second_container}>
+        <div className={styles.second_container_content}>
+          <h2 className="text-preset-2">
+            Not your average book club{" "}
+            <img src={circle} alt="Circle decoration" />
+          </h2>
+          <p className="text-preset-5">
+            Connect with a community that speaks your language - from{" "}
+            <strong>Python</strong> to <strong>TypeScript</strong> and
+            everything in between. Our discussions blend technical depth with
+            practical applications.
+          </p>
+        </div>
+        <img src={notAverageReaders} alt="People enjoying books" />
+      </div>
+    </div>
+  );
+};
+
+export default SecondaryContainer;
